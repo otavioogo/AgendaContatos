ad613c8c501c7c0ceb78dccee3c21604dcc1063b
Otavio Ogo <otavioeidi@hotmail.com>
2022-08-29 19:15:05 -0300

    update

--- agenda/src/main/java/controller/Controller.java
+++ agenda/src/main/java/controller/Controller.java
@@ -7,11 +7,13 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import model.DAO;
+import model.JavaBeans;
 
-@WebServlet(urlPatterns = { "/Controller", "/main" })
+@WebServlet(urlPatterns = { "/Controller", "/main", "/insert" })
 public class Controller extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	DAO dao = new DAO();
+	JavaBeans contato = new JavaBeans();
 
 	public Controller() {
 		super();
@@ -25,15 +27,37 @@ public class Controller extends HttpServlet {
 		System.out.println(action);
 		if (action.equals("/main")) {
 			contatos(request, response);
+		} else if (action.equals("/insert")) {
+			novoContato(request, response);
+		}else {
+			response.sendRedirect("index.html");
 		}
 
 		// teste de conexao
-		dao.testeConexao();
+		
 	}
 
 	// Listar Contatos
 	protected void contatos(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		response.sendRedirect("agenda.jsp");
+	}
+
+	// Novo Contatos
+	protected void novoContato(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		
+	//TESTE
+		System.out.println(request.getParameter("nome"));
+		System.out.println(request.getParameter("fone"));
+		System.out.println(request.getParameter("email"));
+	//SETAR VARIAVEIS JAVABEANS
+		contato.setNome(request.getParameter("nome"));
+		contato.setFone(request.getParameter("fone"));
+		contato.setEmail(request.getParameter("email"));
+	//INVOCAR O METODO INSERIRCONTATO PASSANDO O OBJETO CONTATO
+		dao.inserirContato(contato);
+	//REDIRECIONAR PARA O DOCUMENTO AGENDA.JSP
+		response.sendRedirect("Main");
 	}
 }
--- agenda/src/main/java/model/DAO.java
+++ agenda/src/main/java/model/DAO.java
@@ -2,38 +2,51 @@ package model;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 
 public class DAO {
 	/** MODULO DE CONEXAO **/
 	// PARAMETROS DE CONEXAO
-		private String driver = "com.mysql.cj.jdbc.Driver";
-		private String url = "jdbc:mysql://127.0.0.1:3306/dbagenda?useTimezone=true&serverTimezone=UTC";
-		private String user = "root";
-		private String password = "root";
-		
+	private String driver = "com.mysql.cj.jdbc.Driver";
+	private String url = "jdbc:mysql://127.0.0.1:3306/dbagenda?useTimezone=true&serverTimezone=UTC";
+	private String user = "root";
+	private String password = "root";
+
 	// METODO DE CONEXAO
-		
-		private Connection conectar() {
-			Connection con = null;
-			try {
-				Class.forName(driver);
-				con = DriverManager.getConnection(url, user, password);
-				return con;
-			} catch (Exception e) {
-				System.out.println(e);
-				return null;
-				
-			}
+
+	private Connection conectar() {
+		Connection con = null;
+		try {
+			Class.forName(driver);
+			con = DriverManager.getConnection(url, user, password);
+			return con;
+		} catch (Exception e) {
+			System.out.println(e);
+			return null;
+
 		}
-		// teste de Conexao
-		public void testeConexao() {
-			try {
-				Connection con = conectar();
-				System.out.println(con);
-				con.close();
-			} catch (Exception e) {
-				System.out.println(e);
-			}
+	}
+	/* CRUD CREATE */
+
+	public void inserirContato(JavaBeans contato) {
+		String create = "insert into contatos(nome,fone,email) values(?,?,?)";
+		try {
+			// ABRIR A CONEXAO
+			Connection con = conectar();
+			// Preparar a query para execucao no banco de dados
+			PreparedStatement pst = con.prepareStatement(create);
+			// SUBSTITUIR OS PARAMETROS (?) pelo
+			// CONTEUDO DAS VARIAVEIS JAVABEANS
+			pst.setString(1, contato.getNome());
+			pst.setString(2, contato.getFone());
+			pst.setString(3, contato.getEmail());
+			// EXECUTAR A QUERY
+			pst.executeUpdate();
+			// ENCERRAR A CONEXAO COM O BANCO DE DADOS
+			con.close();
+
+		} catch (Exception e) {
+			System.out.println(e);
 		}
-		
+	}
 }
